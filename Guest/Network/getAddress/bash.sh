bce1ed4df2cb97c4178d622b478b3933c30472d8
DarkStar☀ <61005029+Twin-Flame@users.noreply.github.com>
2023-07-30 14:45:09 -0500

    Update and rename readme.md to bash.sh

--- Guest/Network/getAddress/bash.sh
+++ Guest/Network/getAddress/bash.sh
@@ -1,3 +1,4 @@
 # https://www.cyberciti.biz/faq/find-ip-address-of-linux-kvm-guest-virtual-machine/
+# https://launchcodetechnicaltraining.org/linux/userspace-applications/walkthrough/grep/grep-stdin/index.html
 
 bash = sudo virsh net-dhcp-leases default
